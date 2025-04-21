159958e1d293aa8522f5c37eb9c7b197c7cdf74c
hyein0229 <l1253@naver.com>
2025-04-21 13:18:52 +0900

    [BE] FoodOn spring project setting (S12P31S203-34)

---
+++ FoodOn_backend/src/main/java/com/foodon/foodon/FoodonApplication.java
@@ -0,0 +1,13 @@
+package com.foodon.foodon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodonApplication.class, args);
+	}
+
+}
