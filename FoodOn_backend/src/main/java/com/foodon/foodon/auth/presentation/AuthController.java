d302bd04215cd29e021bcbe5df797376a8408b3d
Pearl-K <rkdwlswn227@gmail.com>
2025-04-24 13:59:24 +0900

    ✨feat: KakaoAuthService, AuthController 구현

---
+++ FoodOn_backend/src/main/java/com/foodon/foodon/auth/presentation/AuthController.java
@@ -0,0 +1,27 @@
+package com.foodon.foodon.auth.presentation;
+
+import com.foodon.foodon.auth.application.KakaoAuthService;
+import com.foodon.foodon.auth.dto.request.KakaoLoginRequest;
+import com.foodon.foodon.auth.dto.response.KakaoLoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final KakaoAuthService kakaoAuthService;
+
+    @PostMapping("/kakao-login")
+    public ResponseEntity<KakaoLoginResponse> loginByKakao(
+            @RequestBody KakaoLoginRequest request
+    ) {
+        KakaoLoginResponse response = kakaoAuthService.loginByKakao(request);
+        return ResponseEntity.ok(response);
+    }
+}
