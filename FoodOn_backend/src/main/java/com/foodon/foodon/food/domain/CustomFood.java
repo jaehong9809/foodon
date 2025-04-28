fa540b96d88a635c33825e13f6f0ea4479ec0c15
hyein0229 <l1253@naver.com>
2025-04-28 17:06:07 +0900

    ✨feat: 멤버, 음식이름 복합인덱스 추가

--- FoodOn_backend/src/main/java/com/foodon/foodon/food/domain/CustomFood.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/food/domain/CustomFood.java
@@ -1,6 +1,7 @@
 package com.foodon.foodon.food.domain;
 
 import com.foodon.foodon.common.entity.BaseTimeEntity;
+import com.foodon.foodon.food.dto.CustomFoodCreateRequest;
 import com.foodon.foodon.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -14,7 +15,9 @@ import static lombok.AccessLevel.PROTECTED;
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-@Table(name = "custom_foods")
+@Table(name = "custom_foods", indexes = {
+        @Index(name = "idx_custom_food_member_food_name", columnList = "member_id, food_name")
+})
 public class CustomFood extends BaseTimeEntity {
 
     @Id
@@ -41,5 +44,32 @@ public class CustomFood extends BaseTimeEntity {
 
     @Embedded
     private Nutrient nutrient;
+
+    private CustomFood(
+            Member member,
+            String name,
+            BigDecimal servingSize,
+            Unit unit,
+            Nutrient nutrient
+    ) {
+        this.member = member;
+        this.name = name;
+        this.servingSize = servingSize;
+        this.unit = unit;
+        this.nutrient = nutrient;
+    }
+
+    public static CustomFood createCustomFoodOfMember(
+            CustomFoodCreateRequest request,
+            Member member
+    ) {
+        return new CustomFood(
+                member,
+                request.foodName(),
+                request.servingSize(),
+                request.unit(),
+                Nutrient.of(request.nutrientInfo())
+        );
+    }
 
 }
