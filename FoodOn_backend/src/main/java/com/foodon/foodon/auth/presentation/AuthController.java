1a91a8d7454afc0c5a10eece1b9ced4eb4a25a32
강진주 <rkdwlswn227@gmail.com>
2025-04-29 13:23:49 +0900

    ✨feat: API Operation(설명) 추가

--- FoodOn_backend/src/main/java/com/foodon/foodon/auth/presentation/AuthController.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/auth/presentation/AuthController.java
@@ -3,6 +3,7 @@ package com.foodon.foodon.auth.presentation;
 import com.foodon.foodon.auth.application.KakaoAuthService;
 import com.foodon.foodon.auth.dto.request.KakaoLoginRequest;
 import com.foodon.foodon.auth.dto.response.KakaoLoginResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ public class AuthController {
     private final KakaoAuthService kakaoAuthService;
 
     @PostMapping("/kakao")
+    @Operation(summary = "카카오 로그인 후 서버 자체 인증 토큰 발급")
     public ResponseEntity<KakaoLoginResponse> loginByKakao(
             @RequestBody KakaoLoginRequest request
     ) {
