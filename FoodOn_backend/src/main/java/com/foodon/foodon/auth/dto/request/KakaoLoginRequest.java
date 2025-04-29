9ae1e1e8f4a1f25a612f53d911c648b03392aba8
강진주 <rkdwlswn227@gmail.com>
2025-04-29 13:13:13 +0900

    ✨feat: Tokens 구조 일원화 (Acs, Rfs 순) - Api 요청, 응답 형식과 순서 통일

--- FoodOn_backend/src/main/java/com/foodon/foodon/auth/dto/request/KakaoLoginRequest.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/auth/dto/request/KakaoLoginRequest.java
@@ -1,9 +1,6 @@
 package com.foodon.foodon.auth.dto.request;
 
 public record KakaoLoginRequest(
-        Long kakaoId,
-        String email,
-        String nickname,
-        String profileImgUrl
+        String accessToken
 ) {
 }
