f449351de17216cec0ada2f4e4e2ef1996ef40df
강진주 <rkdwlswn227@gmail.com>
2025-04-29 15:03:52 +0900

    ✨feat: Auth 도메인 data 계층 구현, dto 구현

--- android-app/app/src/main/java/com/swallaby/foodon/data/auth/remote/api/AuthApi.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/data/auth/remote/api/AuthApi.kt
@@ -1,4 +1,16 @@
 package com.swallaby.foodon.data.auth.remote.api
 
+import com.swallaby.foodon.core.data.remote.BaseResponse
+import com.swallaby.foodon.data.auth.remote.dto.request.KakaoLoginRequest
+import com.swallaby.foodon.data.auth.remote.dto.response.KakaoLoginResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface AuthApi {
+
+    @POST("auth/kakao")
+    suspend fun loginWithKakao(
+        @Body request: KakaoLoginRequest
+    ): BaseResponse<KakaoLoginResponse>
+
 }
---
+++ android-app/app/src/main/java/com/swallaby/foodon/data/auth/remote/dto/response/KakaoLoginResponse.kt
@@ -0,0 +1,6 @@
+package com.swallaby.foodon.data.auth.remote.dto.response
+
+data class KakaoLoginResponse(
+    val accessToken: String,
+    val refreshToken: String
+)
