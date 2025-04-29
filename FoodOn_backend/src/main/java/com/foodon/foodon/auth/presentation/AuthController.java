7643c3208cf6e22f8c0cb7c7704777f64fd7c80f
강진주 <rkdwlswn227@gmail.com>
2025-04-29 13:52:17 +0900

    ✨feat: Response 반환 타입 ResponseUtil.success() 으로 통일

--- FoodOn_backend/src/main/java/com/foodon/foodon/auth/presentation/AuthController.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/auth/presentation/AuthController.java
@@ -3,6 +3,8 @@ package com.foodon.foodon.auth.presentation;
 import com.foodon.foodon.auth.application.KakaoAuthService;
 import com.foodon.foodon.auth.dto.request.KakaoLoginRequest;
 import com.foodon.foodon.auth.dto.response.KakaoLoginResponse;
+import com.foodon.foodon.common.dto.Response;
+import com.foodon.foodon.common.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +22,10 @@ public class AuthController {
 
     @PostMapping("/kakao")
     @Operation(summary = "카카오 로그인 후 서버 자체 인증 토큰 발급")
-    public ResponseEntity<KakaoLoginResponse> loginByKakao(
+    public ResponseEntity<Response<KakaoLoginResponse>> loginByKakao(
             @RequestBody KakaoLoginRequest request
     ) {
         KakaoLoginResponse response = kakaoAuthService.loginByKakao(request.accessToken());
-        return ResponseEntity.ok(response);
+        return ResponseUtil.success(response);
     }
 }
