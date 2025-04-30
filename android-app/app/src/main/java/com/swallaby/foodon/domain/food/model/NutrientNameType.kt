d2852d1277fb319f401a3d12fa5c52a2a472e5f3
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-04-30 09:35:44 +0900

    ✨feat: 영양소 타입 fullName 추가

--- android-app/app/src/main/java/com/swallaby/foodon/domain/food/model/NutrientNameType.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/domain/food/model/NutrientNameType.kt
@@ -6,10 +6,11 @@ import com.swallaby.foodon.core.ui.theme.BGProtein
 import com.swallaby.foodon.core.ui.theme.WB500
 
 enum class NutrientNameType(
+    val fullName: String,
     val shortName: String,
     val color: Color
 ) {
-    CARBOHYDRATE("탄", WB500),
-    PROTEIN("단", BGProtein),
-    FAT("지", BGFat)
+    CARBOHYDRATE("탄수화물", "탄", WB500),
+    PROTEIN("단백질", "단", BGProtein),
+    FAT("지방", "지", BGFat)
 }
