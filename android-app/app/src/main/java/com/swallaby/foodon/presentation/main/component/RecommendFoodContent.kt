1340f5f49b7533a44cf949a2828e0650d0824590
권민채 <minchae075@naver.com>
2025-04-30 16:34:26 +0900

    ✨feat: 메인 콘텐츠 Pager 간격 수정

---
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/main/component/RecommendFoodContent.kt
@@ -0,0 +1,9 @@
+package com.swallaby.foodon.presentation.main.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecommendFoodContent() {
+    Text("이번 주 추천 음식 페이지")
+}
