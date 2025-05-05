d707961fb4e4e4014a5d66ffe54380585c23f4d9
강진주 <rkdwlswn227@gmail.com>
2025-05-05 22:10:07 +0900

    ✨feat: AppDatabase, FoodDao, FoodEntity 구현 - RoomDB 사용을 위한 기본 세팅

---
+++ android-app/app/src/main/java/com/swallaby/foodon/data/food/local/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.swallaby.foodon.data.food.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [LocalFoodEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun foodDao(): FoodDao
+}
