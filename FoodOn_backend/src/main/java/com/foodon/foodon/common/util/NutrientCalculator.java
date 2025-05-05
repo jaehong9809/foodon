a7d53c88d02969cab438844dd37d986ea13a7230
hyein0229 <l1253@naver.com>
2025-05-06 01:43:52 +0900

    ♻️refactor: 영양소 섭취량 총합 구하는 메소드 이름, 로직 리팩토링

--- FoodOn_backend/src/main/java/com/foodon/foodon/common/util/NutrientCalculator.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/common/util/NutrientCalculator.java
@@ -1,8 +1,9 @@
 package com.foodon.foodon.common.util;
 
+import com.foodon.foodon.food.dto.NutrientInfo;
+import com.foodon.foodon.meal.dto.MealNutrientInfo;
 import com.foodon.foodon.nutrientplan.domain.NutrientPlan;
 import com.foodon.foodon.meal.dto.MealItemInfo;
-import com.foodon.foodon.meal.dto.NutrientInfo;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -21,12 +22,16 @@ public class NutrientCalculator {
     /**
      * 영양 성분 섭취량 총합 계산
      */
-    public static BigDecimal sum(
+    public static BigDecimal sumTotalIntake(
             List<MealItemInfo> items,
-            Function<NutrientInfo, BigDecimal> getter
+            Function<MealNutrientInfo, BigDecimal> nutrientGetter
     ) {
         return items.stream()
-                .map(item -> multiply(getter.apply(item.nutrientInfo()), item.quantity()))
+                .map(item -> {
+                    BigDecimal valuePerUnit = nutrientGetter.apply(item.nutrientInfo());
+                    BigDecimal quantity = item.quantity();
+                    return multiply(valuePerUnit, quantity);
+                })
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
