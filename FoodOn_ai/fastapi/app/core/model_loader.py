19ebc6a6e679ae9c024758ddec708fefb95b4f2d
이재홍 <jaehong9809@naver.com>
2025-05-05 17:30:06 +0900

    ✨ feat: faster-rcnn 구조 개발

--- FoodOn_ai/fastapi/app/core/model_loader.py
+++ FoodOn_ai/fastapi/app/core/model_loader.py
@@ -1,11 +1,22 @@
 import torch
-from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import json
+from .food_dataset_name import names
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_model():
-    weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
-    model = fasterrcnn_resnet50_fpn(weights=weights)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(base_dir, "fasterrcnn_food_best.pth")
+    num_classes = len(names) + 1
+
+    model = fasterrcnn_resnet50_fpn(pretrained=False)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     model.eval()
     return model
--- FoodOn_ai/fastapi/app/service/preprocessing.py
+++ FoodOn_ai/fastapi/app/service/preprocessing.py
@@ -1,12 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageOps
 from torchvision import transforms
 import requests
 import torch
 
+# def load_image_from_url(url: str) -> Image.Image:
+#     return Image.open(requests.get(url, stream=True).raw).convert("RGB")
+
+# def preprocess(image: Image.Image, device):
+#     transform = transforms.Compose([transforms.ToTensor()])
+#     img_tensor = transform(image).unsqueeze(0).to(device)
+#     return img_tensor
+
 def load_image_from_url(url: str) -> Image.Image:
-    return Image.open(requests.get(url, stream=True).raw).convert("RGB")
+    image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+    return ImageOps.exif_transpose(image)
 
 def preprocess(image: Image.Image, device):
-    transform = transforms.Compose([transforms.ToTensor()])
+    transform = transforms.Compose([
+        transforms.Resize((512, 512)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
     img_tensor = transform(image).unsqueeze(0).to(device)
     return img_tensor
