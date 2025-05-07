a469d4ff30977aaf2f6574cb64971dfc2e1e43a6
강진주 <rkdwlswn227@gmail.com>
2025-05-07 17:35:24 +0900

    ✨feat: SignUp 흐름에 필요한 UI 구조 잡기

---
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/navigation/SignUpGraph.kt
@@ -0,0 +1,58 @@
+package com.swallaby.foodon.presentation.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.swallaby.foodon.presentation.signup.ActivityTypeScreen
+import com.swallaby.foodon.presentation.signup.GenderScreen
+import com.swallaby.foodon.presentation.signup.GoalWeightScreen
+import com.swallaby.foodon.presentation.signup.ManagementTypeScreen
+import com.swallaby.foodon.presentation.signup.WeightScreen
+import com.swallaby.foodon.presentation.signup.viewmodel.SignUpViewModel
+
+fun NavGraphBuilder.signUpGraph(
+    navController: NavHostController,
+    signUpViewModel: SignUpViewModel
+) {
+    navigation(
+        startDestination = NavRoutes.SignUpGender.route,
+        route = NavRoutes.SignUpGraph.route
+    ) {
+        composable(NavRoutes.SignUpGender.route) {
+            GenderScreen(
+                onNext = { navController.navigate(NavRoutes.SignUpManagement.route) },
+                viewModel = signUpViewModel
+            )
+        }
+        composable(NavRoutes.SignUpManagement.route) {
+            ManagementTypeScreen(
+                onNext = { navController.navigate(NavRoutes.SignUpActivity.route) },
+                viewModel = signUpViewModel
+            )
+        }
+        composable(NavRoutes.SignUpActivity.route) {
+            ActivityTypeScreen(
+                onNext = { navController.navigate(NavRoutes.SignUpWeight.route) },
+                viewModel = signUpViewModel
+            )
+        }
+        composable(NavRoutes.SignUpWeight.route) {
+            WeightScreen(
+                onNext = { navController.navigate(NavRoutes.SignUpGoalWeight.route) },
+                viewModel = signUpViewModel
+            )
+        }
+        composable(NavRoutes.SignUpGoalWeight.route) {
+            GoalWeightScreen(
+                onSubmit = {
+                    //signUpViewModel.submitProfile()
+                    navController.navigate(NavRoutes.MainGraph.route) {
+                        popUpTo(NavRoutes.SignUpGraph.route) { inclusive = true }
+                    }
+                },
+                viewModel = signUpViewModel
+            )
+        }
+    }
+}
---
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/signup/ManagementTypeScreen.kt
@@ -0,0 +1,13 @@
+package com.swallaby.foodon.presentation.signup
+
+import androidx.compose.runtime.Composable
+import com.swallaby.foodon.presentation.signup.viewmodel.SignUpViewModel
+
+
+@Composable
+fun ManagementTypeScreen(
+    onNext: () -> Unit,
+    viewModel: SignUpViewModel
+) {
+
+}
