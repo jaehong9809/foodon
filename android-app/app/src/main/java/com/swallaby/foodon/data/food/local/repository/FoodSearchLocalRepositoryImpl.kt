c072050afa92947eb701f5613aabdb1255a49510
강진주 <rkdwlswn227@gmail.com>
2025-05-07 15:46:13 +0900

    ✨feat: FoodSearch를 위한 data.local 계층의 Mapper와 repoImpl 구현

---
+++ android-app/app/src/main/java/com/swallaby/foodon/data/food/local/repository/FoodSearchLocalRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.swallaby.foodon.data.food.local.repository
+
+import com.swallaby.foodon.data.food.local.FoodSearchDao
+import com.swallaby.foodon.data.food.local.mapper.toDomain
+import com.swallaby.foodon.data.food.local.mapper.toEntity
+import com.swallaby.foodon.domain.food.model.Food
+import com.swallaby.foodon.domain.food.repository.FoodSearchRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+class FoodSearchLocalRepositoryImpl(
+    private val foodSearchDao: FoodSearchDao
+) : FoodSearchRepository {
+
+    override fun searchFoods(query: String): Flow<List<Food>> {
+        return foodSearchDao.searchFoods(query)
+            .map { entityList ->
+                entityList.map { it.toDomain() }
+            }
+    }
+
+    override suspend fun insertAll(foods: List<Food>) {
+        foodSearchDao.insertAll(foods.map { it.toEntity() })
+    }
+
+    override suspend fun clearAll() {
+        foodSearchDao.clearAll()
+    }
+}
