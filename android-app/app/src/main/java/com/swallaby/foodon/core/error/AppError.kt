1d8727ec3272ea7f6a748c11386a7e75eee00c60
강진주 <rkdwlswn227@gmail.com>
2025-05-08 11:16:10 +0900

    ✨feat: AppError 추가 중복된 custom food 이름 등록 불가

--- android-app/app/src/main/java/com/swallaby/foodon/core/error/AppError.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/core/error/AppError.kt
@@ -7,5 +7,6 @@ sealed class AppError(@StringRes val messageRes: Int) {
     object Network : AppError(R.string.error_network)
     object Timeout : AppError(R.string.error_timeout)
     object Unauthorized : AppError(R.string.error_unauthorized)
+    object DuplicateFoodName : AppError(R.string.error_duplicated_name)
     data class Unknown(val throwable: Throwable? = null) : AppError(R.string.error_unknown)
 }
