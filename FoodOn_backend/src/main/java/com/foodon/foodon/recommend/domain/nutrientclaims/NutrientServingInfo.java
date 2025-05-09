8075507be3c1e43c2de98c9932b580d01cf60a6d
이혜인 <l1253@naver.com>
2025-05-09 12:50:53 +0900

    ♻️refactor: 필드 설명 주석 추가

--- FoodOn_backend/src/main/java/com/foodon/foodon/recommend/domain/nutrientclaims/NutrientServingInfo.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/recommend/domain/nutrientclaims/NutrientServingInfo.java
@@ -6,8 +6,8 @@ import java.math.BigDecimal;
 
 public record NutrientServingInfo(
         NutrientCode nutrientCode,
-        BigDecimal per100g,
-        BigDecimal perServing
+        BigDecimal per100g, // 식품 100g 당 수치
+        BigDecimal perServing // 1회 제공량 당 수치
 ) {
 }
 
