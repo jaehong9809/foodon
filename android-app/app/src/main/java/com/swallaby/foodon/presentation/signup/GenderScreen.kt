7309afc94e4316544506dc2065f2af8bbea4453e
강진주 <rkdwlswn227@gmail.com>
2025-05-11 23:50:26 +0900

    ✨feat: 정보 등록 화면 공통 컴포넌트 배치

--- android-app/app/src/main/java/com/swallaby/foodon/presentation/signup/GenderScreen.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/signup/GenderScreen.kt
@@ -1,6 +1,9 @@
 package com.swallaby.foodon.presentation.signup
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.swallaby.foodon.core.ui.component.CommonWideButton
+import com.swallaby.foodon.core.ui.component.OnBoardingTopBar
 import com.swallaby.foodon.presentation.signup.viewmodel.SignUpViewModel
 
 
@@ -10,4 +13,17 @@ fun GenderScreen(
     viewModel: SignUpViewModel
 ) {
 
+    OnBoardingTopBar(
+        modifier = Modifier,
+        curIdx = 1,
+        total = 5,
+        onBackClick = { }
+    )
+
+    CommonWideButton(
+        modifier = Modifier,
+        text = "다음",
+        isEnabled = true,
+        onClick = onNext
+    )
 }
