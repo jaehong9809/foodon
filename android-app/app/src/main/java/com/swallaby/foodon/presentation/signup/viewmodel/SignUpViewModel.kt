eacad34639c43f54d4e39729aaba7f170575f5ca
강진주 <rkdwlswn227@gmail.com>
2025-05-12 11:21:11 +0900

    ✨feat: SignUpViewModel updateState 공통 메서드 구현

--- android-app/app/src/main/java/com/swallaby/foodon/presentation/signup/viewmodel/SignUpViewModel.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/signup/viewmodel/SignUpViewModel.kt
@@ -1,13 +1,19 @@
 package com.swallaby.foodon.presentation.signup.viewmodel
 
 import com.swallaby.foodon.core.presentation.BaseViewModel
+import com.swallaby.foodon.presentation.calendar.viewmodel.CalendarUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
 
 ) : BaseViewModel<SignUpUiState>(SignUpUiState()) {
+
+    fun updateState(block: (SignUpUiState) -> SignUpUiState) {
+        _uiState.update(block)
+    }
 
     fun selectGender(gender: String) {
         updateState { it.copy(selectedGender = gender) }
