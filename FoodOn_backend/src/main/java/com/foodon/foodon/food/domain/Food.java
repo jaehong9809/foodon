7a05b63d95c8f83a3706c58ace251a3852bba901
이혜인 <l1253@naver.com>
2025-05-12 12:50:32 +0900

    ✨feat: foodType, displayName, 검색 여부 필드 추가 및 수정 시 생성 메소드 추가

--- FoodOn_backend/src/main/java/com/foodon/foodon/food/domain/Food.java
+++ FoodOn_backend/src/main/java/com/foodon/foodon/food/domain/Food.java
@@ -2,15 +2,18 @@ package com.foodon.foodon.food.domain;
 
 import com.foodon.foodon.food.dto.CustomFoodCreateRequest;
 import com.foodon.foodon.meal.domain.MealItem;
+import com.foodon.foodon.meal.dto.MealItemInfo;
 import com.foodon.foodon.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.foodon.foodon.food.domain.FoodType.PUBLIC;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -19,8 +22,8 @@ import static lombok.AccessLevel.PROTECTED;
 @Table(
         name = "foods",
         indexes = {
-            @Index(name = "idx_food_name", columnList = "food_name"),
-            @Index(name = "idx_food_member_name", columnList = "member_id, food_name")
+                @Index(name = "idx_food_name", columnList = "food_name"),
+                @Index(name = "idx_food_member_name", columnList = "member_id, food_name")
         }
 )
 public class Food {
@@ -32,8 +35,15 @@ public class Food {
 
     private Long memberId; // 커스텀
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FoodType foodType = PUBLIC;
+
     @Column(name = "food_name", unique = true, nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false, length = 100)
+    private String displayName;
 
     @Column(length = 100)
     private String category;
@@ -45,28 +55,55 @@ public class Food {
     @Column(nullable = false)
     private Unit unit;
 
+    @Column(nullable = false)
+    private boolean searchable;
 
     private Food(
             Long memberId,
+            FoodType foodType,
             String name,
+            String displayName,
             BigDecimal servingSize,
-            Unit unit
+            Unit unit,
+            boolean searchable
     ){
         this.memberId = memberId;
+        this.foodType = foodType;
         this.name = name;
+        this.displayName = displayName;
         this.servingSize = servingSize;
         this.unit = unit;
+        this.searchable = searchable;
     }
 
     public static Food createCustomFoodByMember(
-        CustomFoodCreateRequest request,
-        Member member
+            CustomFoodCreateRequest request,
+            Member member
     ) {
         return new Food(
                 member.getId(),
+                FoodType.CUSTOM,
+                request.foodName(),
                 request.foodName(),
                 request.servingSize(),
-                request.unit()
+                request.unit(),
+                true
+        );
+    }
+
+    public static Food createCustomFoodModifiedByMember(
+            CustomFoodCreateRequest request,
+            String registerName,
+            Member member
+    ){
+        return new Food(
+                member.getId(),
+                FoodType.CUSTOM_MODIFIED,
+                registerName,
+                request.foodName(),
+                request.servingSize(),
+                request.unit(),
+                false
         );
     }
 
