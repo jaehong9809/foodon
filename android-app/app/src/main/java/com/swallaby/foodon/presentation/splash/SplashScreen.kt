9522ab2bc41aa66efdf17208242c2ec839190a48
강진주 <rkdwlswn227@gmail.com>
2025-05-12 17:24:00 +0900

    ✨feat: SplashScreen 화면 구현

---
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/splash/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.swallaby.foodon.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.swallaby.foodon.R
+import com.swallaby.foodon.core.ui.theme.WB500
+
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = WB500),
+        contentAlignment = Alignment.Center
+    ) {
+        // TODO: 추후 권한 얻으면 로고에 JSON 애니메이션 추가
+
+        Image(
+            painter = painterResource(id = R.drawable.logo_splash),
+            contentDescription = "foodOn Logo",
+            modifier = Modifier.size(188.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview(){
+    SplashScreen()
+}
