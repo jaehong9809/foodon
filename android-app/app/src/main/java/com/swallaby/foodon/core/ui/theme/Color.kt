65dfa77b33bf1d08fde1f5ff5e9759d56455004e
강진주 <rkdwlswn227@gmail.com>
2025-05-13 11:28:19 +0900

    💄style: Color, Style 설정 추가 및 수정

--- android-app/app/src/main/java/com/swallaby/foodon/core/ui/theme/Color.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/core/ui/theme/Color.kt
@@ -6,10 +6,6 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val MainWhite = Color(0xFFFFFFFF)
 val MainBlack = Color(0xFF000000)
 
@@ -67,3 +63,6 @@ val TextRed = Color(0xFFF63454)
 
 /* Kakao Color */
 val KakaoYellow = Color(0xFFFEE500)
+
+/* Wheel Color */
+val BGWheel = Color(0xFFEEEEEE)
