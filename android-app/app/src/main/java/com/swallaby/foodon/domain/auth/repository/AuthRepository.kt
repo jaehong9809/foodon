b391daabdd9468c8661888d8a706da0b0a1f878f
강진주 <rkdwlswn227@gmail.com>
2025-05-15 14:36:41 +0900

    ✨feat: 자동로그인을 위한 토큰 검증 Domain 계층 구현

--- android-app/app/src/main/java/com/swallaby/foodon/domain/auth/repository/AuthRepository.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/domain/auth/repository/AuthRepository.kt
@@ -2,8 +2,10 @@ package com.swallaby.foodon.domain.auth.repository
 
 import com.swallaby.foodon.core.result.ApiResult
 import com.swallaby.foodon.data.auth.remote.dto.response.KakaoLoginResponse
+import com.swallaby.foodon.data.auth.remote.dto.response.TokenResponse
 
 
 interface AuthRepository {
     suspend fun loginWithKakao(accessToken: String) : ApiResult<KakaoLoginResponse>
+    suspend fun validateToken(accessToken: String, refreshToken: String): ApiResult<TokenResponse>
 }
