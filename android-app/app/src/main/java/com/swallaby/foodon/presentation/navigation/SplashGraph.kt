6a7e6169d0f837f5020fd85f34adadd8a6f41369
강진주 <rkdwlswn227@gmail.com>
2025-05-15 10:43:25 +0900

    ✨feat: 시스템 Splash 화면 띄우기 완료

--- android-app/app/src/main/java/com/swallaby/foodon/presentation/navigation/SplashGraph.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/navigation/SplashGraph.kt
@@ -3,18 +3,22 @@ package com.swallaby.foodon.presentation.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.swallaby.foodon.presentation.splash.SplashScreen
 
-fun NavGraphBuilder.splashGraph(
-    navController: NavHostController
-) {
-    composable(route = NavRoutes.Splash.route) {
-        SplashScreen(
-            onNavigate = { destination ->
-                navController.navigate(destination.route) {
-                    popUpTo(NavRoutes.Splash.route) { inclusive = true }
+fun NavGraphBuilder.splashGraph(navController: NavHostController) {
+    navigation(
+        startDestination = NavRoutes.Splash.route,
+        route = NavRoutes.SplashGraph.route
+    ) {
+        composable(route = NavRoutes.Splash.route) {
+            SplashScreen(
+                onNavigate = { destination ->
+                    navController.navigate(destination.route) {
+                        popUpTo(NavRoutes.SplashGraph.route) { inclusive = true }
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }
