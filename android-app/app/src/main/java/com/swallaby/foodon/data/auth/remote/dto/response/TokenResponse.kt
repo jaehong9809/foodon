6fc05c57762ca6e991c5538142853fa9de776caa
강진주 <rkdwlswn227@gmail.com>
2025-05-15 20:55:34 +0900

    ✨feat: TokenResponse dto 필드 추가 - profile 관련 값도 함께 받아서 화면 분기

--- android-app/app/src/main/java/com/swallaby/foodon/data/auth/remote/dto/response/TokenResponse.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/data/auth/remote/dto/response/TokenResponse.kt
@@ -2,5 +2,6 @@ package com.swallaby.foodon.data.auth.remote.dto.response
 
 data class TokenResponse(
     val accessToken: String,
-    val refreshToken: String
+    val refreshToken: String,
+    val profileUpdated: Boolean
 )
