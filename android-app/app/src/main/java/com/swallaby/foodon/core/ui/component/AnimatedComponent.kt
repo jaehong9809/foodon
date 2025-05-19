97a16e6e98af27867ad241c16f7b7d4c0f79aea7
김정현 <invent819@naver.com>
2025-05-19 14:29:26 +0900

    ✨feat: 음식 리스트 slider 애니메이션 추가

--- android-app/app/src/main/java/com/swallaby/foodon/core/ui/component/AnimatedComponent.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/core/ui/component/AnimatedComponent.kt
@@ -3,8 +3,10 @@ package com.swallaby.foodon.core.ui.component
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideInVertically
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -41,3 +43,30 @@ fun BouncingAnimatedComponent(
         content()
     }
 }
+
+@Composable
+fun VerticalSlideAnimatedComponent(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    var visible by remember { mutableStateOf(false) }
+    LaunchedEffect(Unit) {
+        delay(100)
+        visible = true
+    }
+
+    AnimatedVisibility(
+        visible = visible, enter = fadeIn(
+            animationSpec = tween(
+                durationMillis = 400
+            )
+        ) + slideInVertically(
+            initialOffsetY = { it / 5 }, animationSpec = tween(
+                durationMillis = 400
+            )
+        ), modifier = modifier
+    ) {
+        content()
+    }
+}
+
