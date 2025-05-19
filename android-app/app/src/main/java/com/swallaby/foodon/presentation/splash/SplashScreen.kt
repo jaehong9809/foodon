e8481f5ff17a424ae379dcc1c6bc3737a63db4f6
강진주 <rkdwlswn227@gmail.com>
2025-05-20 00:11:50 +0900

    ✨feat: Splash 화면 애니메이션 적용

--- android-app/app/src/main/java/com/swallaby/foodon/presentation/splash/SplashScreen.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/splash/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.swallaby.foodon.presentation.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +9,18 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.swallaby.foodon.R
 import com.swallaby.foodon.core.result.ResultState
 import com.swallaby.foodon.core.ui.theme.WB500
 import com.swallaby.foodon.data.auth.remote.result.AuthFlowResult
 import com.swallaby.foodon.presentation.navigation.NavRoutes
 import com.swallaby.foodon.presentation.splash.viewmodel.SplashViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 
 @Composable
@@ -27,15 +28,15 @@ fun SplashScreen(
     onNavigate : (NavRoutes) -> Unit,
     viewModel: SplashViewModel = hiltViewModel()
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = WB500),
-        contentAlignment = Alignment.Center
-    ) {
-        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-        LaunchedEffect(uiState.result) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.Asset("SSAFY-Splash-Logo.json")
+    )
+    val lottieAnimState = animateLottieCompositionAsState(composition, iterations = 1)
+
+    LaunchedEffect(lottieAnimState.isAtEnd) {
+        if (lottieAnimState.isAtEnd) {
             when (val result = uiState.result) {
                 is ResultState.Success -> {
                     when (result.data) {
@@ -46,14 +47,20 @@ fun SplashScreen(
                 is ResultState.Error -> {
                     onNavigate(NavRoutes.LoginGraph)
                 }
-                else -> Unit // Loading 상태일 때 대기, 추후 ProgressBar or Animation 추가 가능
+                else -> Unit
             }
         }
+    }
 
-        // TODO: 추후 권한 얻으면 로고에 JSON 애니메이션 추가
-        Image(
-            painter = painterResource(id = R.drawable.logo_splash),
-            contentDescription = "foodOn Logo",
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = WB500),
+        contentAlignment = Alignment.Center
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = { lottieAnimState.progress },
             modifier = Modifier.size(188.dp)
         )
     }
