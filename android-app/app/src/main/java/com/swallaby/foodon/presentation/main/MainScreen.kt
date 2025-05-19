f1daa5204014dd41ec80b61cb5bbca7be8bcd098
강진주 <rkdwlswn227@gmail.com>
2025-05-19 10:53:08 +0900

    ✨feat: foods.db 파일 탑재

--- android-app/app/src/main/java/com/swallaby/foodon/presentation/main/MainScreen.kt
+++ android-app/app/src/main/java/com/swallaby/foodon/presentation/main/MainScreen.kt
@@ -1,10 +1,7 @@
 package com.swallaby.foodon.presentation.main
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
@@ -25,7 +22,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.swallaby.foodon.BuildConfig
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.swallaby.foodon.R
 import com.swallaby.foodon.core.result.ResultState
